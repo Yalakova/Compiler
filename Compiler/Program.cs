f8d1e8c7cb2d08fee3915b898c25e5ed4a94927b
Asus <Asus@DESKTOP-GQP2S9I>
2025-02-13 18:51:19 +0300

    Добавилена задача

--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -10,7 +10,30 @@ namespace Compiler
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число для вычисления факториала:");
+            string input = Console.ReadLine();
 
+            if (int.TryParse(input, out int number) && number >= 0)
+            {
+                long factorial = CalculateFactorial(number);
+                Console.WriteLine($"Факториал числа {number} равен {factorial}.");
+            }
+            else
+            {
+                Console.WriteLine("Пожалуйста, введите неотрицательное целое число.");
+            }
+            Console.ReadLine();
         }
+
+        static long CalculateFactorial(int n)
+        {
+            long result = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    
     }
 }
